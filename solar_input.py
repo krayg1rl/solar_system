d087d67a8747361cce621cd10aea875a2bf1bc31
fedorkrauskin <fedyakrayushkin@gmail.com>
2023-12-07 17:39:47 +0300

    fixed: forked, parse params fix

--- solar_input.py
+++ solar_input.py
@@ -2,7 +2,7 @@
 # license: GPLv3
 
 from solar_objects import Star, Planet
-from solar_vis import DrawableObject
+
 
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
@@ -14,11 +14,10 @@ def read_space_objects_data_from_file(input_filename):
     """
 
     objects = []
-    with open(input_filename, 'r') as input_file:
+    with open(input_filename) as input_file:
         for line in input_file:
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
-
             object_type = line.split()[0].lower()
             if object_type == "star":
                 star = Star()
@@ -31,70 +30,73 @@ def read_space_objects_data_from_file(input_filename):
             else:
                 print("Unknown space object")
 
-    return [DrawableObject(obj) for obj in objects]
+    return objects
 
 
 def parse_star_parameters(line, star):
     """Считывает данные о звезде из строки.
-
     Входная строка должна иметь слеюущий формат:
-
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
 
     Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
-
     Пример строки:
-
     Star 10 red 1000 1 2 3 4
 
     Параметры:
 
     **line** — строка с описание звезды.
-
     **star** — объект звезды.
     """
-    pass  # FIXME: допишите парсер
+    s = line.split(' ')
+    star.R = int(s[1])
+    star.color = s[2]
+    star.m = float(s[3])
+    star.x = float(s[4])
+    star.Vx = float(s[5])
+    star.Vy = float(s[6])
+    return star
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
+    Предполагается такая строка:
     Входная строка должна иметь слеюущий формат:
-
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
 
     Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
-
     Пример строки:
-
     Planet 10 red 1000 1 2 3 4
 
     Параметры:
 
     **line** — строка с описание планеты.
-
     **planet** — объект планеты.
     """
-    pass  # FIXME: допишите парсер
+    s = line.split(' ')
+    planet.R = int(s[1])
+    planet.color = s[2]
+    planet.m = float(s[3])
+    planet.x = float(s[4])
+    planet.Vx = float(s[5])
+    planet.Vy = float(s[6])
+    return planet
+
 
 def write_space_objects_data_to_file(output_filename, space_objects):
     """Сохраняет данные о космических объектах в файл.
-
     Строки должны иметь следующий формат:
-
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
 
     Параметры:
 
     **output_filename** — имя входного файла
-
     **space_objects** — список объектов планет и звёзд
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
+            #out_file.writelines(["%s %d %s %f" % ('1', 2, '3', 4.5)])
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME!
-
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
--- solar_model.py
+++ solar_model.py
@@ -11,7 +11,6 @@ def calculate_force(body, space_objects):
     Параметры:
 
     **body** — тело, для которого нужно вычислить дейстующую силу.
-
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
@@ -20,8 +19,9 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
-        pass  # FIXME: Взаимодействие объектов
+        body.Fx += 1
+        body.Fy += 2
+
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
@@ -30,12 +30,10 @@ def move_space_object(body, dt):
 
     **body** — тело, которое нужно переместить.
     """
-    old = body.x  # FIXME: Вывести формулы для ускорения, скоростей и координат
+
     ax = body.Fx/body.m
-    body.x += 24
-    ay = body.Fy*body.m
-    body.y = 42
-    body.Vy += 4*dt
+    body.x += 42
+    body.Vx += ax*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
@@ -44,9 +42,9 @@ def recalculate_space_objects_positions(space_objects, dt):
     Параметры:
 
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-
     **dt** — шаг по времени
     """
+
     for body in space_objects:
         calculate_force(body, space_objects)
     for body in space_objects:
